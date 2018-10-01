88ca943a08f86927a8c6b22aafd7b5c9b4aaa1a5
muddge <muddge@gmail.com>
2018-10-01 00:02:51 -0700

    Add files via upload

---
+++ data-pipeline-kafka-spark/file-pipeline-util/src/main/java/com/muddge/messaging/KafkaEventProducer.java
@@ -0,0 +1,52 @@
+
+package com.muddge.messaging;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+public class KafkaEventProducer {
+
+	private final KafkaProducer<String, String> kafkaProducer;
+
+	public KafkaEventProducer(String kafkaBrokerUrl) {
+		Properties properties = new Properties();
+		properties.put("bootstrap.servers", kafkaBrokerUrl);
+		properties.put("acks", "all");
+		properties.put("retries", 0);
+		properties.put("batch.size", 16384);
+		properties.put("linger.ms", 1);
+		properties.put("buffer.memory", 33554432);
+		properties.put("key.serializer",
+				"org.apache.kafka.common.serialization.StringSerializer");
+		properties.put("value.serializer",
+				"org.apache.kafka.common.serialization.StringSerializer");
+
+		kafkaProducer = new KafkaProducer<>(properties);
+	}
+
+	public void send(Event event) {
+		Future<RecordMetadata> responseFuture = kafkaProducer
+				.send(new ProducerRecord<String, String>(event.getCode(), event
+						.getFileName(), event.toString()));
+		// force delivery
+		try {
+			responseFuture.get(3, TimeUnit.SECONDS);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+//	public static void main(String[] args) {
+//		new KafkaEventProducer("quickstart.cloudera:9092").send(new Event(
+//				"file-ingestion-error", "2018-01-01-15",
+//				"Error occured trying to upload the fileKey the hdfs"));
+//	}
+
+}
